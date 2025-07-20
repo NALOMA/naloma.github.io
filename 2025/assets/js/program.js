48a088d5b49ce62cec4ac28c06bc9d6e2c0e21d7
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2025-07-20 14:03:14 +0200

    newline in program
    making newline work in program titles

--- 2025/assets/js/program.js
+++ 2025/assets/js/program.js
@@ -29,14 +29,14 @@ document.addEventListener('DOMContentLoaded', function() {
         eventClassNames: function(info) { return 'fc-event-' + info.event.extendedProps.type; },
         events: [
             { title: 'Introduction', start: '2025-08-04T17:00', end: '2025-08-04T17:05', extendedProps: { type: 'special' } },
-            { title: 'Keynote:\nAaron Steven White\nTBA', start: '2025-08-04T17:05', end: '2025-08-04T18:00', extendedProps: { type: 'keynote', link: 'keynote1' } },
+            { title: 'Keynote: Aaron Steven White\nTBA', start: '2025-08-04T17:05', end: '2025-08-04T18:00', extendedProps: { type: 'keynote', link: 'keynote1' } },
             { title: 'In the Mood for Inference: Logic-Based NLI with LLMs', start: '2025-08-04T18:00', end: '2025-08-04T18:25', extendedProps: { type: 'paper', link: 'talk4' } },
 
             { title: 'MERGE: Minimal Expression-Replacement GEneralization Test for NLI', start: '2025-08-05T17:00', end: '2025-08-05T17:25', extendedProps: { type: 'contributed', link: 'talk6' } },
             { title: 'Implementing a Logical Inference System for Japanese Comparatives', start: '2025-08-05T17:25', end: '2025-08-05T17:55', extendedProps: { type: 'paper', link: 'talk1' } },
             { title: 'Dataset Creation for Visual Entailment using Generative AI', start: '2025-08-05T17:55', end: '2025-08-05T18:20', extendedProps: { type: 'paper', link: 'talk3' } },
 
-            { title: 'Keynote:\nMehrnoosh Sadrzadeh\nTBA', start: '2025-08-06T17:00', end: '2025-08-06T17:55', extendedProps: { type: 'keynote', link: 'keynote2' } },
+            { title: 'Keynote: Mehrnoosh Sadrzadeh\nTBA', start: '2025-08-06T17:00', end: '2025-08-06T17:55', extendedProps: { type: 'keynote', link: 'keynote2' } },
             { title: 'Unpacking Legal Reasoning in LLMs: Chain-of-Thought as a Key to Human-Machine Alignment in Essay-Based NLU Tasks', start: '2025-08-06T17:55', end: '2025-08-06T18:20', extendedProps: { type: 'paper', link: 'talk2' } },
 
             
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
             { title: 'Automatic Evaluation of Linguistic Validity in Japanese CCG Treebanks', start: '2025-08-07T17:25', end: '2025-08-07T17:50', extendedProps: { type: 'contributed', link: 'talk7' } },
             { title: 'How Often does Natural Logic Actually Meet Machine Learning?', start: '2025-08-07T17:50', end: '2025-08-07T18:15', extendedProps: { type: 'contributed', link: 'talk8' } },
 
-            { title: 'Keynote:\nKyle Richardson\nTBA', start: '2025-08-08T17:00', end: '2025-08-08T17:55', extendedProps: { type: 'keynote', link: 'keynote3' } }
+            { title: 'Keynote: Kyle Richardson\nTBA', start: '2025-08-08T17:00', end: '2025-08-08T17:55', extendedProps: { type: 'keynote', link: 'keynote3' } }
         ]
     });
     calendar.render();
