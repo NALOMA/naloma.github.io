69112c4e075c27e0241f439b5adf86388decfdce
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2025-07-21 15:26:32 +0200

    swap talks
    swapping two talks

--- 2025/assets/js/program.js
+++ 2025/assets/js/program.js
@@ -32,9 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
             { title: 'Keynote: Aaron Steven White\nTBA', start: '2025-08-04T17:05', end: '2025-08-04T18:00', extendedProps: { type: 'keynote', link: 'keynote1' } },
             { title: 'In the Mood for Inference: Logic-Based NLI with LLMs', start: '2025-08-04T18:00', end: '2025-08-04T18:25', extendedProps: { type: 'paper', link: 'talk4' } },
 
-            { title: 'MERGE: Minimal Expression-Replacement GEneralization Test for NLI', start: '2025-08-05T17:00', end: '2025-08-05T17:25', extendedProps: { type: 'contributed', link: 'talk6' } },
+            { title: 'Dataset Creation for Visual Entailment using Generative AI', start: '2025-08-05T17:00', end: '2025-08-05T17:25', extendedProps: { type: 'paper', link: 'talk3' } },
             { title: 'Implementing a Logical Inference System for Japanese Comparatives', start: '2025-08-05T17:25', end: '2025-08-05T17:55', extendedProps: { type: 'paper', link: 'talk1' } },
-            { title: 'Dataset Creation for Visual Entailment using Generative AI', start: '2025-08-05T17:55', end: '2025-08-05T18:20', extendedProps: { type: 'paper', link: 'talk3' } },
+            { title: 'MERGE: Minimal Expression-Replacement GEneralization Test for NLI', start: '2025-08-05T17:55', end: '2025-08-05T18:20', extendedProps: { type: 'contributed', link: 'talk6' } },
 
             { title: 'Keynote: Mehrnoosh Sadrzadeh\nTBA', start: '2025-08-06T17:00', end: '2025-08-06T17:55', extendedProps: { type: 'keynote', link: 'keynote2' } },
             { title: 'Unpacking Legal Reasoning in LLMs: Chain-of-Thought as a Key to Human-Machine Alignment in Essay-Based NLU Tasks', start: '2025-08-06T17:55', end: '2025-08-06T18:20', extendedProps: { type: 'paper', link: 'talk2' } },
