c5fa8fc31dcdba21b9cf6c618640b40c32d9192b
kovvalsky <lasha.abzianidze@gmail.com>
2025-08-01 23:51:34 +0200

    rearrangment in program

--- 2025/assets/js/program.js
+++ 2025/assets/js/program.js
@@ -29,23 +29,27 @@ document.addEventListener('DOMContentLoaded', function() {
         eventClassNames: function(info) { return 'fc-event-' + info.event.extendedProps.type; },
         events: [
             { title: 'Introduction', start: '2025-08-04T17:00', end: '2025-08-04T17:05', extendedProps: { type: 'special' } },
-            { title: 'Keynote: Aaron Steven White\nTBA', start: '2025-08-04T17:05', end: '2025-08-04T18:00', extendedProps: { type: 'keynote', link: 'keynote1' } },
+            { title: 'Keynote: Aaron Steven White\nUnderstanding Complex Situation Descriptions', start: '2025-08-04T17:05', end: '2025-08-04T18:00', extendedProps: { type: 'keynote', link: 'keynote1' } },
             { title: 'In the Mood for Inference: Logic-Based NLI with LLMs', start: '2025-08-04T18:00', end: '2025-08-04T18:25', extendedProps: { type: 'paper', link: 'talk4' } },
 
-            { title: 'Dataset Creation for Visual Entailment using Generative AI', start: '2025-08-05T17:00', end: '2025-08-05T17:25', extendedProps: { type: 'paper', link: 'talk3' } },
-            { title: 'Implementing a Logical Inference System for Japanese Comparatives', start: '2025-08-05T17:25', end: '2025-08-05T17:55', extendedProps: { type: 'paper', link: 'talk1' } },
-            { title: 'MERGE: Minimal Expression-Replacement GEneralization Test for NLI', start: '2025-08-05T17:55', end: '2025-08-05T18:20', extendedProps: { type: 'contributed', link: 'talk6' } },
+            // { title: 'Dataset Creation for Visual Entailment using Generative AI', start: '2025-08-05T17:00', end: '2025-08-05T17:25', extendedProps: { type: 'paper', link: 'talk3' } },
+            // { title: 'Implementing a Logical Inference System for Japanese Comparatives', start: '2025-08-05T17:25', end: '2025-08-05T17:55', extendedProps: { type: 'paper', link: 'talk1' } },
+            // { title: 'MERGE: Minimal Expression-Replacement GEneralization Test for NLI', start: '2025-08-05T17:55', end: '2025-08-05T18:20', extendedProps: { type: 'contributed', link: 'talk6' } },
+            { title: 'Implementing a Logical Inference System for Japanese Comparatives', start: '2025-08-05T17:00', end: '2025-08-05T17:30', extendedProps: { type: 'paper', link: 'talk1' } },
+            { title: 'MERGE: Minimal Expression-Replacement GEneralization Test for NLI', start: '2025-08-05T17:30', end: '2025-08-05T17:55', extendedProps: { type: 'contributed', link: 'talk6' } },
 
-            { title: 'Keynote: Mehrnoosh Sadrzadeh\nTBA', start: '2025-08-06T17:00', end: '2025-08-06T17:55', extendedProps: { type: 'keynote', link: 'keynote2' } },
+            { title: 'Keynote: Mehrnoosh Sadrzadeh\nHow can large language model become more human?', start: '2025-08-06T17:00', end: '2025-08-06T17:55', extendedProps: { type: 'keynote', link: 'keynote2' } },
             { title: 'Unpacking Legal Reasoning in LLMs: Chain-of-Thought as a Key to Human-Machine Alignment in Essay-Based NLU Tasks', start: '2025-08-06T17:55', end: '2025-08-06T18:20', extendedProps: { type: 'paper', link: 'talk2' } },
 
+            { title: 'Dataset Creation for Visual Entailment using Generative AI', start: '2025-08-07T17:00', end: '2025-08-07T17:25', extendedProps: { type: 'paper', link: 'talk3' } },
+            { title: 'Building a Compact Math Corpus', start: '2025-08-07T17:25', end: '2025-08-07T17:50', extendedProps: { type: 'paper', link: 'talk5' } },
+            { title: 'Automatic Evaluation of Linguistic Validity in Japanese CCG Treebanks', start: '2025-08-07T17:50', end: '2025-08-07T18:15', extendedProps: { type: 'contributed', link: 'talk7' } },
             
-            { title: 'Building a Compact Math Corpus', start: '2025-08-07T17:00', end: '2025-08-07T17:25', extendedProps: { type: 'paper', link: 'talk5' } },
-            { title: 'Automatic Evaluation of Linguistic Validity in Japanese CCG Treebanks', start: '2025-08-07T17:25', end: '2025-08-07T17:50', extendedProps: { type: 'contributed', link: 'talk7' } },
-            { title: 'How Often does Natural Logic Actually Meet Machine Learning?', start: '2025-08-07T17:50', end: '2025-08-07T18:15', extendedProps: { type: 'contributed', link: 'talk8' } },
 
-            { title: 'Keynote: Kyle Richardson\nTBA', start: '2025-08-08T17:00', end: '2025-08-08T17:55', extendedProps: { type: 'keynote', link: 'keynote3' } },
-            { title: 'Closing', start: '2025-08-08T17:55', end: '2025-08-08T18:00', extendedProps: { type: 'special' } },
+            { title: 'Keynote: Kyle Richardson\nUnderstanding the Logic of Generative AI through Logic', start: '2025-08-08T17:00', end: '2025-08-08T17:55', extendedProps: { type: 'keynote', link: 'keynote3' } },
+            { title: 'How Often does Natural Logic Actually Meet Machine Learning?', start: '2025-08-08T17:55', end: '2025-08-08T18:20', extendedProps: { type: 'contributed', link: 'talk8' } },
+            // { title: 'Closing', start: '2025-08-08T17:55', end: '2025-08-08T18:00', extendedProps: { type: 'special' } },
+            { title: 'Closing', start: '2025-08-08T18:20', end: '2025-08-08T18:25', extendedProps: { type: 'special' } },
         ]
     });
     calendar.render();
