efe8d0bab850fd1ef168e29ca2c307aebaba6f83
kovvalsky <lasha.abzianidze@gmail.com>
2025-08-04 16:18:43 +0200

    add paper links

--- 2025/assets/js/program.js
+++ 2025/assets/js/program.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         eventClassNames: function(info) { return 'fc-event-' + info.event.extendedProps.type; },
         events: [
-            { title: 'Introduction', start: '2025-08-04T17:00', end: '2025-08-04T17:05', extendedProps: { type: 'special' } },
+            { title: 'Opening', start: '2025-08-04T17:00', end: '2025-08-04T17:05', extendedProps: { type: 'special' } },
             { title: 'Keynote: Aaron Steven White\nUnderstanding Complex Situation Descriptions', start: '2025-08-04T17:05', end: '2025-08-04T18:00', extendedProps: { type: 'keynote', link: 'keynote1' } },
             { title: 'In the Mood for Inference: Logic-Based NLI with LLMs', start: '2025-08-04T18:00', end: '2025-08-04T18:25', extendedProps: { type: 'paper', link: 'talk4' } },
 
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
             { title: 'Implementing a Logical Inference System for Japanese Comparatives', start: '2025-08-05T17:00', end: '2025-08-05T17:30', extendedProps: { type: 'paper', link: 'talk1' } },
             { title: 'MERGE: Minimal Expression-Replacement GEneralization Test for NLI', start: '2025-08-05T17:30', end: '2025-08-05T17:55', extendedProps: { type: 'contributed', link: 'talk6' } },
 
-            { title: 'Keynote: Mehrnoosh Sadrzadeh\nHow can large language model become more human?', start: '2025-08-06T17:00', end: '2025-08-06T17:55', extendedProps: { type: 'keynote', link: 'keynote2' } },
+            { title: 'Keynote: Mehrnoosh Sadrzadeh\nHow Can Large Language Model Become More Human?', start: '2025-08-06T17:00', end: '2025-08-06T17:55', extendedProps: { type: 'keynote', link: 'keynote2' } },
             { title: 'Unpacking Legal Reasoning in LLMs: Chain-of-Thought as a Key to Human-Machine Alignment in Essay-Based NLU Tasks', start: '2025-08-06T17:55', end: '2025-08-06T18:20', extendedProps: { type: 'paper', link: 'talk2' } },
 
             { title: 'Dataset Creation for Visual Entailment using Generative AI', start: '2025-08-07T17:00', end: '2025-08-07T17:25', extendedProps: { type: 'paper', link: 'talk3' } },
